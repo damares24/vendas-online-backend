fc5cf204ee5923b48b8b3473f65c5fe30fcccc9f
damares24 <damares290619@gmail.com>
2023-02-13 12:49:05 -0300

    Criar banco de dados no docker com postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      // type: 'postgres',
+      // host: process.env.DB_HOST,
+      // port: Number(process.env.DB_PORT),
+      // username: process.env.DB_USERNAME,
+      // password: String(process.env.DB_PASSWORD),
+      // database: process.env.DB_DATABASE,
+      type: "postgres",
+      host:"localhost",
+      port: 5432,
+      username: "postgres",
+      password: "mysecretpassword",
+      database: "vendasOnline",
+    }),
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
