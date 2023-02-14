88c66de1ec71f50b3ebeffe0a735c8de55753f31
damares24 <damares290619@gmail.com>
2023-02-14 16:02:55 -0300

    Create DTO using pipe

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,7 +1,18 @@
+import { IsString } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
     name: string;
+
+    @IsString()
     email: string;
+
+    @IsString()
     phone: string;
+
+    @IsString()
     cpf: number;
+
+    @IsString()
     password: string;
 }
