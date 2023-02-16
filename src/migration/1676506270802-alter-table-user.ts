e57efa6e89ebab8c53bc72d356a8c8f111bd7166
damares24 <damares290619@gmail.com>
2023-02-15 21:22:54 -0300

    Insert validation in email duplicated

---
+++ src/migration/1676506270802-alter-table-user.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class alterTableUser1676506270802 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+            alter table public.user add unique(email);
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+            
+        `)
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadGatewayException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
@@ -12,6 +12,11 @@ export class UserService {
         private readonly userRepository: Repository<UserEntity>,
     ){}
     async createUser(createUserDto : CreateUserDto): Promise<UserEntity>{
+        const user = await this.findUserByEmail(createUserDto.email).catch(() => undefined);
+
+        if (user) {
+            throw new BadGatewayException('Email registered in system')
+        }
         const saltOrRounds = 10;
 
         const passwordHashed = await hash(createUserDto.password, saltOrRounds);
