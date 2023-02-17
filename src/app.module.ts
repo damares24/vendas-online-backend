0c80a6566a64e3ce23112158174f0faf53280a22
damares24 <damares290619@gmail.com>
2023-02-17 08:50:24 -0300

    Insert unit tests in state and city

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,6 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './guards/roles.guards';
 import { JwtModule } from '@nestjs/jwt';
-import { User2Service } from './user2/user2.service';
 
 
 @Module({
@@ -49,7 +48,6 @@ import { User2Service } from './user2/user2.service';
       provide: APP_GUARD,
       useClass: RolesGuard,
     },
-    User2Service,
   ],
 })
 export class AppModule {}
---
+++ src/city/__mocks__/city.mock.ts
@@ -0,0 +1,10 @@
+import { stateMock } from "../../state/__mocks__/state.mock";
+import { CityEntity } from "../entities/city.entity";
+
+export const cityMock: CityEntity = {
+    created_at: new Date(),
+    id: 456666665,
+    name: 'cityMock',
+    state_id: stateMock.id,
+    updated_at: new Date(),
+}
