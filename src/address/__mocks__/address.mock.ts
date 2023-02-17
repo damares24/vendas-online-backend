e5f28d5544e12f75d73da24835d220b27682cb79
damares24 <damares290619@gmail.com>
2023-02-17 09:24:55 -0300

    Insert unit test in addressService

---
+++ src/address/__mocks__/address.mock.ts
@@ -0,0 +1,14 @@
+import { cityMock } from "../../city/__mocks__/city.mock";
+import { AddressEntity } from "../entities/address.entity";
+import { UserEntityMock } from "../../user/__mocks__/user.mock";
+
+export const addressMock: AddressEntity = {
+    cep: '4546645',
+    city_id: cityMock.id,
+    complement: 'complement',
+    created_at: new Date(),
+    id: 45645,
+    numberAddress: 599,
+    updated_at: new Date(),
+    user_id: UserEntityMock.id,
+}
