11ade7ae6ddb9cda984dcfe129bf01828f74a12a
damares24 <damares290619@gmail.com>
2023-02-17 20:38:29 -0300

    Create categoryService

---
+++ src/category/__mocks__/category.mock.ts
@@ -0,0 +1,8 @@
+import { CategoryEntity } from "../entities/category.entity";
+
+export const categoryMock: CategoryEntity = {
+    created_at: new Date(),
+    id: 5463,
+    name: 'categoryMock',
+    updated_at: new Date(),
+}
--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,4 +1,4 @@
-import { ProductEntity } from "src/product/entities/product.entity";
+import { ProductEntity } from "../../product/entities/product.entity";
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({ name: 'category'})
@@ -16,5 +16,5 @@ export class CategoryEntity {
     updated_at: Date;
 
     @OneToMany(() => ProductEntity, (product: ProductEntity) =>product.category)
-    products: ProductEntity;
+    products?: ProductEntity;
 }
