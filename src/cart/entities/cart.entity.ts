01d0ff9073f6c2e2202278445707ecb03eb6bdf5
damares24 <damares290619@gmail.com>
2023-02-21 21:01:50 -0300

    Create cart entity

---
+++ src/cart/entities/cart.entity.ts
@@ -0,0 +1,15 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class CartEntity {
+    @PrimaryGeneratedColumn('rowid')
+    id: number;
+    
+    @Column({ name: 'user_id', nullable: false})
+    userId: string;
+
+    @CreateDateColumn({ name: 'created_at'})
+    created_at: Date;
+
+    @UpdateDateColumn({ name: 'updated_at'})
+    updated_at: Date;
+}
