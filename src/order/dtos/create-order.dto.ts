adeee19c79c4a86011e36fb51c73eff81f59aeb1
damares24 <damares290619@gmail.com>
2023-02-27 20:38:15 -0300

    Create order controller

---
+++ src/order/dtos/create-order.dto.ts
@@ -0,0 +1,16 @@
+
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateOrderDTO {
+  @IsOptional()
+  @IsNumber()
+  amountPayments?: number;
+
+  @IsOptional()
+  @IsString()
+  codePix?: string;
+
+  @IsOptional()
+  @IsString()
+  datePayment?: string;
+}
